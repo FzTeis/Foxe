25ace627bea9dbb95aeb2665e4b2f3fee0cb97a0
Ado <meadonix@gmail.com>
2026-05-24 15:26:05 -0600

    Crear config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+const config = {
+  bot_name: 'Foxe Assistant',
+  bot_author: 'thisAdo, FzTeis, IamDestroy',
+
+  owners: [
+    '50498273976',
+    '522431268546',
+    '573196588149'
+  ],
+
+  prefix: ['.', '!', '#'],
+
+  messages: {
+    owner: 'Dueño solamente.',
+    group: 'Grupos solamente.',
+    admin: 'Admins solamente.',
+    bot_admin: 'Hazme admin primero.'
+  }
+}
+
+export default config
